4611756ee3688d1b1ef8161eb3fb9a42110dd368
杨景渊 <oneonetwo@users.noreply.github.com>
2020-04-07 17:53:57 +0800

    Create jingdian.js

---
+++ jingdian.js
@@ -0,0 +1,10 @@
+###正则
+> 1. 处理cookie
+```javascript
+$(document).ready(function () {
+    alert('RUNOOB');
+});
+```
+      ```javascript 
+        document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
+      ```
