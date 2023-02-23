2059c24cd162e6c461d8ba2c70d964d49218cbb9
yangjingyuan <yangjingyuan@pwrd.com>
2023-02-23 13:06:54 +0800

    files: 设计模式添加单例中express用户缓存的示例

---
+++ 设计模式/Single/express的用户缓存.ts
@@ -0,0 +1,28 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: D
+ * @Date: 2023-02-23 13:02:52
+ * @LastEditors: jy
+ * @LastEditTime: 2023-02-23 13:05:59
+ */
+let express = require('express');
+let app = express();
+let fs = require('fs');
+//缓存一定要做成单例的，不管谁写进去了，别人都可以拿到
+let cache: Record<string, any> = {};
+app.get('/api/users/:id', function (req: any, res: any) {
+    console.log(cache);
+    let id = req.params.id;
+    if (cache[id]) {
+        res.json(cache[id]);
+    } else {
+        fs.readFile(`./users/${id}.json`, 'utf8', (err: any, data: any) => {
+            let user = JSON.parse(data);
+            cache[id] = user;
+            res.json(user);
+        });
+    }
+
+});
+app.listen(8080);
