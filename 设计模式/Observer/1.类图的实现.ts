b823eb138c70d27eb35762bff5136242c8596520
jingyuan.yang <jingyuan.yang@prnasia.com>
2023-02-26 22:13:29 +0800

    files: 设计模式 观察者模式 initial

---
+++ 设计模式/Observer/1.类图的实现.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Author: yjy
+ * @Date: 2023-02-26 21:52:17
+ * @LastEditTime: 2023-02-26 21:52:28
+ * @LastEditors: yjy
+ * @Reference: 
+ */
+abstract class Student { 
+    abstract update(): void;
+}
