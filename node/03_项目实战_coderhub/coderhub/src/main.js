55cb202772a99c6401a07dbefbb9ba5b014e98dc
yangjingyuan <yangjingyuan@pwrd.com>
2024-06-13 19:52:14 +0800

    feat: vue-project

--- node/03_项目实战_coderhub/coderhub/src/main.js
+++ node/03_项目实战_coderhub/coderhub/src/main.js
@@ -1,11 +1,24 @@
 const KoaRouter = require('@koa/router')
 const app = new (require('koa'))
+const cors = require('@koa/cors')
 
+
+app.use(cors({
+    origin: 'http://localhost:5173',// 允许的源
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowHeaders: ['Content-type', 'Authorization'],
+    credentials: true,
+    maxAge: 86400
+}))
 
 const userRouter = new KoaRouter({ prefix: '/users' });
 
-userRouter.get('/list', (ctx, next) => { 
-    ctx.body = 'user list'
+userRouter.get('/login', (ctx, next) => { 
+    ctx.status = 200
+    ctx.body = {
+        code: 0,
+        message: '请求失败'
+    }
 })
 
 app.use(userRouter.routes())
