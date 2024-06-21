c21c9b40f3d13988b6e59377e02c8f18bc901304
yangjingyuan <yangjingyuan@pwrd.com>
2024-06-21 10:46:13 +0800

    feat: node coderhub 动态 评论的接口完程

--- node/03_项目实战_coderhub/coderhub/src/service/moment.service.js
+++ node/03_项目实战_coderhub/coderhub/src/service/moment.service.js
@@ -22,21 +22,37 @@ class MomentService{
     // },
     async queryList(limit=10, offset=0){
         // const statement = 'SELECT * FROM `moment` LIMIT ? OFFSET ?;'
-        const statement = "SELECT m.id, m.content, m.createAt createTime, m.updateAt updateTime, JSON_OBJECT('id', u.id, 'name', u.name, 'createTime', u.createAt, 'updateTime', u.updateAt) AS user FROM `moment` m LEFT JOIN `user` u ON  m.user_id = u.id LIMIT ? OFFSET ?"
+        // const statement = "SELECT m.id, m.content, m.createAt createTime, m.updateAt updateTime, JSON_OBJECT('id', u.id, 'name', u.name, 'createTime', u.createAt, 'updateTime', u.updateAt) AS user FROM `moment` m LEFT JOIN `user` u ON  m.user_id = u.id LIMIT ? OFFSET ?"
+        const statement = `SELECT m.id, m.content, m.createAt createTime, m.updateAt updateTime, 
+            JSON_OBJECT('id', u.id, 'name', u.name) AS user,
+            (SELECT COUNT(*) FROM comment c WHERE m.id=c.moment_id) commentCount
+            FROM moment m 
+            LEFT JOIN user u ON  m.user_id = u.id 
+            LIMIT ? OFFSET ?;`
         //这两个必须是字符串。
         const [rows, fields] = await connection.execute(statement, [String(limit), String(offset)])
         return rows
     }
 
     async queryById(id){
-        const statement = `SELECT m.id, m.content, m.createAt createTime, m.updateAt updateTime, 
-        JSON_OBJECT('id', u.id, 'name', u.name, 'createTime', u.createAt, 'updateTime', u.updateAt) AS user 
+        const statement = `SELECT m.id, m.content, m.createAt createTime, m.updateAt updateTime,
+        JSON_OBJECT('id', u.id, 'name', u.name) AS users, 
+        (JSON_ARRAYAGG(JSON_OBJECT(
+            'id', c.id, 
+            'content', c.content, 
+            'commentId', c.comment_id, 
+            'momentId', c.moment_id, 
+            'user',  JSON_OBJECT('id', cu.id, 'name', cu.name)
+        )))  commentList
         FROM moment m 
         LEFT JOIN user u ON  m.user_id = u.id
+        LEFT JOIN comment c ON m.id=c.moment_id
+        LEFT JOIN user cu ON cu.id=c.user_id
         WHERE m.id=?
+        GROUP BY m.id;
         `
         //这两个必须是字符串。
-        const [rows, fields] = await connection.execute(statement, [String(id)])
+        const [rows, fields] = await connection.execute(statement, [id])
         return rows
     }
     async update(content, momentId){
